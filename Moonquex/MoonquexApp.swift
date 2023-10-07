2bd25a0ea1c3e33156a6b00c0880297918ea06c2
Krishna Babani <57276165+babanikrishna@users.noreply.github.com>
2023-10-07 16:25:46 -0700

    Initial Commit

---
+++ Moonquex/MoonquexApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonquexApp.swift
+//  Moonquex
+//
+//  Created by Krishna Babani on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonquexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
