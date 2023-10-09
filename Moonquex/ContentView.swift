9bad20ba67e9a9e322d000c6a73979eaffab0a29
Krishna Babani <57276165+babanikrishna@users.noreply.github.com>
2023-10-08 18:54:26 -0700

    V1 (Build 2)

--- Moonquex/ContentView.swift
+++ Moonquex/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     @State private var selectedMoonquake: MoonQuakes?
     @State private var showLatLongLines: Bool = false
     @State private var showMoonquakes: Bool = false
-    @State private var showLunarData: Bool = true
+    @State private var showLunarData: Bool = false
     
     var body: some View {
         VStack {
@@ -32,52 +32,59 @@ struct ContentView: View {
                                 }
             )
             .sheet(item: $selectedMoonquake) { moonquake in
-                VStack() {
+                ScrollView {
+                    
+                    HStack {
                     
                     VStack(alignment: .leading) {
-                    VStack(alignment: .leading) {
-                        Text("Moonquake Data")
-                            .font(.title)
-                            .fontWeight(.bold)
-                        Text("Station: \(moonquake.station)")
-                            .fontWeight(.semibold)
-                        Text("Category: \(moonquake.category)")
-                            .fontWeight(.semibold)
+                        VStack(alignment: .leading) {
+                            Text("Moonquake Data")
+                                .font(.title)
+                                .fontWeight(.bold)
+                            Text("Station: \(moonquake.station)")
+                                .fontWeight(.semibold)
+                            Text("Category: \(moonquake.category)")
+                                .fontWeight(.semibold)
+                        }
+                        .padding(.horizontal, 20)
+                        .padding(.bottom, 10)
+                        
+                        VStack(alignment: .leading) {
+                            Text("Location: \(moonquake.latitude), \(moonquake.longitude)")
+                            Text("Date: \(moonquake.date)")
+                                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                            Text("Depth: \(moonquake.depth ?? 0.0)")
+                            Text("Magnitude: \(moonquake.magnitude ?? 0.0)")
+                        }
+                        .padding(.horizontal, 20)
                     }
-                    .padding(.horizontal, 20)
-                    .padding(.bottom, 10)
-                    
-                    VStack(alignment: .leading) {
-                        Text("Location: \(moonquake.latitude), \(moonquake.longitude)")
-                        Text("Date: \(moonquake.date)")
-                        Text("Depth: \(moonquake.depth ?? 0.0)")
-                        Text("Magnitude: \(moonquake.magnitude ?? 0.0)")
-                    }
-                    .padding(.horizontal, 20)
+                        
+                        Spacer()
                 }
                     .padding(.top, 20)
                     
                     ZStack(alignment: .leading) {
-                                    LinearGradient(
-                                        gradient: Gradient(colors: [.blue, .green, .yellow, .red]),
-                                        startPoint: .leading,
-                                        endPoint: .trailing
-                                    )
-                                    .frame(height: 20)
-                                    .cornerRadius(10)
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .strokeBorder(Color.black, lineWidth: 1)
-                                    )
-                                    
-                                    Rectangle()
-                                        .fill(Color.black)
-                                        .frame(width: 2, height: 20)
-                                        .offset(x: CGFloat((moonquake.magnitude ?? 0.0) / 10.0) * (UIScreen.main.bounds.width - 40) - 1)
-                                }
+                        LinearGradient(
+                            gradient: Gradient(colors: [.blue, .green, .yellow, .red]),
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        )
+                        .frame(height: 20)
+                        .cornerRadius(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .strokeBorder(Color.primary, lineWidth: 1)
+                        )
+                        
+                        Rectangle()
+                            .fill(Color.primary)
+                            .frame(width: 2, height: 20)
+                            .offset(x: CGFloat((moonquake.magnitude ?? 0.0) / 3.5) * (UIScreen.main.bounds.width - 40) - 1)
+                    }
+
                                 .padding(.horizontal, 20)
                 }
-                .presentationDetents([.fraction(0.3), .medium])
+                .presentationDetents([.fraction(0.4), .medium])
             }
             .edgesIgnoringSafeArea(.all)
         }
